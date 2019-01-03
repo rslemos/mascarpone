4fac5abfba1414adca2c8b762f217b148edb3463
R. Lemos <rslemos@gmail.com>
2019-01-02 23:52:28 -0200

    $ ng generate directive onBlur --project=mascarpone

--- projects/mascarpone/src/lib/mascarpone.module.ts
+++ projects/mascarpone/src/lib/mascarpone.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { OnBlurDirective } from './on-blur.directive';
 
 @NgModule({
   imports: [
   ],
   declarations: [
+    OnBlurDirective
   ],
   exports: [
   ],
