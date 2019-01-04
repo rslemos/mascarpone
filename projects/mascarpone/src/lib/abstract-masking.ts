cbe30436336d712f377d3cd57d88cd5042cd4d87
R. Lemos <rslemos@gmail.com>
2019-01-04 09:27:58 -0200

    mask w/ automatic selection handling [lcs]
    Useless for mask on blur (though harmless too).

--- projects/mascarpone/src/lib/abstract-masking.ts
+++ projects/mascarpone/src/lib/abstract-masking.ts
@@ -2,6 +2,7 @@ import { Inject } from '@angular/core';
 
 import { MASK } from './mask-fn';
 import { Mask } from './mask-fn';
+import { makeFull } from './lcs';
 
 export abstract class AbstractMaskingDirective {
   constructor(
@@ -13,17 +14,26 @@ export abstract class AbstractMaskingDirective {
     const target = <HTMLInputElement>event.target;
 
     // extract data
-    const {value} = target;
+    const {value, selectionStart, selectionEnd, selectionDirection} = target;
 
     // apply masking
-    const result = this._mask.mask(value);
+    const result0 = this._mask.mask(value);
+    const result = makeFull(value, result0, selectionStart, selectionEnd, selectionDirection);
 
-    if (target.value === result) {
+    if (target.value === result.value &&
+      target.selectionStart === result.selectionStart &&
+      target.selectionEnd === result.selectionEnd &&
+      target.selectionDirection === result.selectionDirection) {
       return;
     }
 
     // inject data
-    target.value = result;
+    ({
+        value: target.value,
+        selectionStart: target.selectionStart,
+        selectionEnd: target.selectionEnd,
+        selectionDirection: target.selectionDirection
+    } = result);
 
     // value/selection changed; redispatch event
     target.dispatchEvent(event);
