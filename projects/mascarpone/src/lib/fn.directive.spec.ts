d1a8f143f80b3d0f22ff39e248d1d7c7921861f0
R. Lemos <rslemos@gmail.com>
2019-01-06 18:47:51 -0200

    $ ng generate directive fn --project=mascarpone

---
+++ projects/mascarpone/src/lib/fn.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FnDirective } from './fn.directive';
+
+describe('FnDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FnDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- projects/mascarpone/src/lib/mascarpone.module.ts
+++ projects/mascarpone/src/lib/mascarpone.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { OnBlurDirective } from './on-blur.directive';
+import { FnDirective } from './fn.directive';
 
 @NgModule({
   imports: [
   ],
   declarations: [
     OnBlurDirective,
+    FnDirective,
   ],
   exports: [
     OnBlurDirective,
