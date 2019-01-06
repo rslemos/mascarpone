17c1cf48cbea1f59e1b404b909b34165b8f3ce41
R. Lemos <rslemos@gmail.com>
2019-01-06 18:46:45 -0200

    mask on input w/o selection handling

--- projects/mascarpone/src/lib/mascarpone.module.ts
+++ projects/mascarpone/src/lib/mascarpone.module.ts
@@ -14,6 +14,7 @@ import { OnInputDirective } from './on-input.directive';
   exports: [
     MascarponeComponent,
     OnBlurDirective,
+    OnInputDirective,
   ]
 })
 export class MascarponeModule { }
