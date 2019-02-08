fe9543b8237580bdc8b111ba73ad8160beab189f
R. Lemos <rslemos@gmail.com>
2019-02-08 10:08:32 -0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { MASK } from 'mascarpone';
+import { Mask } from 'mascarpone';
+
+class MyMask implements Mask {
+  public mask(value: string): string {
+    console.error(`mask: ${value}`);
+    return value;
+  }
+}
 
 @Component({
   selector: 'mask-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // providers: [
+  //   {provide: MASK, useClass: MyMask, multi: false},
+  // ]
 })
 export class AppComponent {
   title = 'mascarpone-demo';
+
+  public myMaskingFn(value: string): string {
+    console.error(`mask: ${value}`, this);
+    return value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MascarponeModule } from 'mascarpone';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MascarponeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
